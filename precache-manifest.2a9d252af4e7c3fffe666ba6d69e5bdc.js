f8ed4f7c37a42c4433b19357139dc0eee37b85ca
megzimo <40956115+megzimo@users.noreply.github.com>
2018-12-17 14:50:42 -0600

    Updates

---
+++ precache-manifest.2a9d252af4e7c3fffe666ba6d69e5bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f5b86e359b6dc1ccd44",
+    "url": "/React-tac-toe/static/js/runtime~main.4f5b86e3.js"
+  },
+  {
+    "revision": "e9a1b47fc76f3f27dd31",
+    "url": "/React-tac-toe/static/js/main.e9a1b47f.chunk.js"
+  },
+  {
+    "revision": "5bc363a705c279ce0d67",
+    "url": "/React-tac-toe/static/js/1.5bc363a7.chunk.js"
+  },
+  {
+    "revision": "e9a1b47fc76f3f27dd31",
+    "url": "/React-tac-toe/static/css/main.bdf3db04.chunk.css"
+  },
+  {
+    "revision": "a20e2907aaa54c468d1132a5f679e52f",
+    "url": "/React-tac-toe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3f2def20cb81e711cf3f05f4cff650c.js"
+  "/React-tac-toe/precache-manifest.2a9d252af4e7c3fffe666ba6d69e5bdc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-tac-toe/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
